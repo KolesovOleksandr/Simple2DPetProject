91fbc56792d94e541fb3ce04a9039e428b65d10d
OleksandrKolesov <Kolesov.Oleksandr@fivesysdev.com>
2025-12-16 00:02:33 +0200

    Add working damage system

--- Assets/Scripts/Character/PlayerHealth.cs
+++ Assets/Scripts/Character/PlayerHealth.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
 
-public class PlayerHealth
+public class PlayerHealth: MonoBehaviour
 {
-    
+    private int maxHealth = 100;
+    private int health;
+
+    void Awake()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log($"Player HP: {health}");
+
+        if (health <= 0)
+        {
+            Debug.Log("Player died!");
+            
+            gameObject.SetActive(false);
+        }
+    }
 }
